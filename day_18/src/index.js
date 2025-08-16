51c6cc7941b9c977059b58f27947f86a02a66730
AkashUmair152 <Akashumair922@Gmail.com>
2025-08-16 09:29:43 +0500

    📅 day_19 ✅ ApiErrorHandler class ✅ApiResponse Class ✅ and setting up the app.js with some packeges { cookie-parser, cors, }

--- day_18/src/index.js
+++ day_18/src/index.js
@@ -1,10 +1,19 @@
 // require('dotenv').config({path: "./env"}) 
 import dotenv from "dotenv";
 import connectDb from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env",
 })
 
 
-connectDb();
+connectDb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is Runing at the Port: ${process.env.PORT}` );
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed", err);
+})
