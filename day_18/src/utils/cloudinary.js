77c027998678ce0275b403aa46bc5761b94d60b7
AkashUmair152 <Akashumair922@Gmail.com>
2025-08-20 06:45:59 +0500

    📅 day_20 setiting up multer cloundinary, and routes for users

---
+++ day_18/src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs";
+
+    cloudinary.config({ 
+        cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key:process.env.CLOUDINARY_API_KEY, 
+        api_secret:process.env.CLOUDINARY_API_SECRET
+    });
+
+export const uploadImageOnCloudinry = async (filePath) => {
+    if (!filePath) return null;
+    try {
+        const result = await cloudinary.uploader.upload(filePath, {
+            folder: 'public', // Optional: specify a folder in Cloudinary
+            resource_type: 'auto' // Automatically determine the resource type (image, video, etc.)
+        });
+        console.log("Image uploaded successfully:", result.secure_url || result.url);
+        
+        fs.unlinkSync(filePath); // Remove the file after upload
+        return result;
+    } catch (error) {
+        console.error("Error uploading image to Cloudinary:", error);
+        throw error;
+    }
+}
